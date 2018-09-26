82d6ee6ee935303ec739581a2ad40684a6cd97c7
samu18p <42410016+samu18p@users.noreply.github.com>
2018-09-25 22:02:07 -0500

    Add files via upload

---
+++ Nodo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct nodo{
+	int Val;
+		struct nodo *ptr;
+};
+int main(){
+struct nodo *inicio; 
+struct nodo *nodo;
+
+nodo=new struct nodo;
+inicio=nodo;
+inicio->ptr=NULL;
+inicio->Val=10;
+
+nodo=new struct nodo;
+inicio->ptr=nodo;
+inicio->ptr->ptr=NULL;
+inicio->ptr->Val=20;
+
+cout<<inicio->Val;
+cout<<inicio->ptr->Val;
+
+return 0;
+}
+
+
+# Nodo
